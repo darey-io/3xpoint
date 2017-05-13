70c4b132e68c7591a3c511ca7e45e5280ce18526
data-engineer-dare <dare.david.olufunmilayo@gmail.com>
2017-05-13 16:05:50 +0100

    module 1 SQL files completed

---
+++ module1/CustomerAW.sql
@@ -0,0 +1,13 @@
+USE XPOINT
+GO
+
+CREATE TABLE CustomerAW (
+CustomerID INT,
+FirstName varchar (50),
+MiddleName varchar (50),
+LastName varchar (50),
+CompanyName varchar (50),
+EmailAddress nVarchar (50) 
+);
+
+
